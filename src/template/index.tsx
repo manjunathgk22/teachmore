5f1f33ebe19656acc0599cc1fd83f97c06ae9ad8
manjunath <manjunathgk22@gmail.com>
2025-02-19 17:16:46 +0530

    responsive template

--- src/template/index.tsx
+++ src/template/index.tsx
@@ -1,47 +1,61 @@
-import React from 'react'
+"use client"
 
-const Layout = ({...props}) => {
+import type * as React from "react"
+
+import { Sidebar, SidebarContent, SidebarHeader, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className='flex w-full h-full bg-background'>
-        <SideBar />
-        <div className="flex flex-col flex-1">
-            <Header />
-        <main className='flex flex-col items-center justify-center flex-1 overflow-auto'>
-            {props.children}
-        </main>
+    <SidebarProvider>
+      <div className="flex h-full min-h-screen w-full bg-background">
+        <AppSidebar />
+        <div className="flex w-full flex-1 flex-col">
+          <Header />
+          <main className="flex flex-1 flex-col items-center justify-center overflow-auto">{children}</main>
         </div>
-    </div>
+      </div>
+    </SidebarProvider>
   )
 }
 
-export default Layout
-
 const Header = () => {
-    return <div className='border-b flex h-[72px]'>Header</div>
+  return (
+    <header className="flex h-[72px] items-center border-b px-4">
+      <SidebarTrigger className="md:hidden" />
+      <div className="ml-4">Header</div>
+    </header>
+  )
 }
 
-const SideBar = () => {
-    return (
-        <div className='w-[150px] h-full bg-background border-r'>
-        <div className='flex flex-col items-center justify-center'>
-            <div className='w-1/2 h-1/2 bg-white rounded-full'></div>
-            <div className='flex flex-col items-center justify-center'>
-            <h1>sidebar</h1>
-            </div>
+const AppSidebar = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader className="border-b p-4">
+        <div className="flex flex-col items-center justify-center">
+          <div className="aspect-square w-16 rounded-full bg-muted" />
+          <h1 className="mt-2 text-sm font-medium">Sidebar</h1>
         </div>
-        </div>
-    )
+      </SidebarHeader>
+      <SidebarContent>{/* Add your sidebar content here */}</SidebarContent>
+    </Sidebar>
+  )
 }
 
 const withLayout = (Component: React.ComponentType) => {
-    const WrappedComponent: React.FC = (props) => (
-        <Layout>
-            <Component {...props} />
-        </Layout>
-    );
-    WrappedComponent.displayName = `withLayout(${(Component.displayName ?? Component.name) || 'Component'})`;
-    return WrappedComponent;
+  const WrappedComponent: React.FC = (props) => (
+    <Layout>
+      <Component {...props} />
+    </Layout>
+  )
+
+  WrappedComponent.displayName = `withLayout(${Component.displayName ?? (Component.name || "Component")})`
+
+  return WrappedComponent
 }
 
-export { withLayout }
+export { Layout, withLayout }
 
